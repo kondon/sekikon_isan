aeb20d7831ecc763622c0ce45642aa7eba44da99
sekiya <sekiya@sekiya-no-MacBook-Pro.local>
2015-07-15 21:46:26 +0900

    ui_js変更

--- public/js/ui_item.js
+++ public/js/ui_item.js
@@ -1,3 +1,5 @@
+
+ var param = {};
 $(function(){
  console.log('ui_item.js in');
 
@@ -11,7 +13,6 @@ $(function(){
 
  // 追加ボタン（index.htmlのid=add）押下時 実行
  $("#add").click(function(e){ e.preventDefault();
- var param = {};
  param.item1 = $("#item1").val() || "";
  param.test0 = Math.floor(Math.random()*100);
  param.test1 = Math.floor(Math.random()*100);
@@ -79,12 +80,12 @@ $(function(){
 
  // sekiya 全件表示ボタン（index.htmlのid=getAll）押下時 実行
 
- console.log('DEBUG param.item1 : '+param.item1);
- console.log('DEBUG param.test0 : '+param.test0);
- console.log('DEBUG param.test1 : '+param.test1);
- console.log('DEBUG param.test2 : '+param.test2);
- console.log('DEBUG param.test3 : '+param.test3);
  $("#cheack").click(function(e){ e.preventDefault();
+   console.log('DEBUG param.item1 : '+param.item1);
+   console.log('DEBUG param.test0 : '+param.test0);
+   console.log('DEBUG param.test1 : '+param.test1);
+   console.log('DEBUG param.test2 : '+param.test2);
+   console.log('DEBUG param.test3 : '+param.test3);
 
    // POSTでのajaxコールで、サーバーのapp.jsのapp.post /getAll呼び出し
    $.ajax({
